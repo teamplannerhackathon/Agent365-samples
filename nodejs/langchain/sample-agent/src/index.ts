e733e9caa8a754d49ed56b8d871f33a86cc50194
Jesus Daniel Terrazas <96103167+JesuTerraz@users.noreply.github.com>
2025-11-07 14:51:49 -0800

    Update Agents SDK for LangChain Sample (#15)
    * update agents sdk version for langchain
    
    * Accept more recent versions
    
    * remove authority field, this is set by default
    
    ---------
    
    Co-authored-by: Jesus Terrazas <jterrazas@microsoft.com>

--- nodejs/langchain/sample-agent/src/index.ts
+++ nodejs/langchain/sample-agent/src/index.ts
@@ -4,36 +4,29 @@ import { configDotenv } from 'dotenv';
 configDotenv();
 
 import { AuthConfiguration, authorizeJWT, CloudAdapter, Request } from '@microsoft/agents-hosting';
-import express, { Response } from 'express';
+import express, { Response } from 'express'
 import { agentApplication } from './agent';
 
 const authConfig: AuthConfiguration = {};
-const adapter = new CloudAdapter(authConfig);
 
-const app = express();
-app.use(express.json());
-app.use(authorizeJWT(authConfig));
+const server = express()
+server.use(express.json())
+server.use(authorizeJWT(authConfig))
 
-app.post('/api/messages', async (req: Request, res: Response) => {
-  await adapter.process(req, res, async (context) => {
-    await agentApplication.run(context);
-  });
-});
+server.post('/api/messages', (req: Request, res: Response) => {
+  const adapter = agentApplication.adapter as CloudAdapter;
+  adapter.process(req, res, async (context) => {
+    await agentApplication.run(context)
+  })
+})
 
-const port = process.env.PORT || 3978;
-const server = app.listen(port, () => {
-  console.log(`\nServer listening to port ${port} for appId ${authConfig.clientId} debug ${process.env.DEBUG}`);
+const port = process.env.PORT || 3978
+server.listen(port, async () => {
+  console.log(`\nServer listening to port ${port} for appId ${authConfig.clientId} debug ${process.env.DEBUG}`)
 }).on('error', async (err) => {
   console.error(err);
   process.exit(1);
 }).on('close', async () => {
-  console.log('Kairo is shutting down...');
-});
-
-process.on('SIGINT', () => {
-  console.log('Received SIGINT. Shutting down gracefully...');
-  server.close(() => {
-    console.log('Server closed.');
-    process.exit(0);
-  });
+  console.log('Server closed');
+  process.exit(0);
 });
