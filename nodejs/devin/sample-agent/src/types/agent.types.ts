705f73fbdc5ad7da6a96f62e8791e647a05490a3
Walter Luna <walterluna@microsoft.com>
2025-11-18 13:35:19 +0000

    add copyright header

--- nodejs/devin/sample-agent/src/types/agent.types.ts
+++ nodejs/devin/sample-agent/src/types/agent.types.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { DefaultConversationState, TurnState } from "@microsoft/agents-hosting";
 
 interface ConversationState extends DefaultConversationState {
